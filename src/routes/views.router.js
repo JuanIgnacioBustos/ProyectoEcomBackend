eca4640188a45b0e188bbbccbbe5b8e08502ed58
juan.ignaciobustos7@gmail.com <juan.ignaciobustos7@gmail.com>
2023-08-01 23:41:10 -0300

    Empiezo a trabajar en la vista de products sin agregarle la funcionalidad al boton de agregar al carrito

--- src/routes/views.router.js
+++ src/routes/views.router.js
@@ -12,14 +12,27 @@ router.get('/', async (req, res) => {
         title: "Inicio",
         products: products.docs
     });
-})
+    })
 
-router.get('/realtimeproducts', async (req, res) => {
+    router.get('/realtimeproducts', async (req, res) => {
     res.render('realTimeProducts');
 })
 
 router.get('/chat', async (req, res) => {
     res.render('chat')
+})
+
+router.get('/products', async (req, res) => {
+    let page = req.query.page
+
+    let products = await productManager.getProducts(2, page, undefined);
+    products.prevLink = products.hasPrevPage?`http://localhost:8080/products?page=${products.prevPage}`:'';
+    products.nextLink = products.hasNextPage?`http://localhost:8080/products?page=${products.nextPage}`:'';
+    
+    res.render('products', {
+        title: "Products",
+        products: products
+    })
 })
 
 export default router;
