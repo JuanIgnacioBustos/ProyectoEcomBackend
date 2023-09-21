081df0bb1fd9c18614be90173de942dfed79f9ba
juan.ignaciobustos7@gmail.com <juan.ignaciobustos7@gmail.com>
2023-09-21 13:03:55 -0300

    Actualizo detalles del carrito en los metodos de compra de los productos

--- src/services/cart.service.js
+++ src/services/cart.service.js
@@ -66,7 +66,6 @@ export default class CartService {
         let product = await this.productService.getProductById(prod.product._id)
 
         if (prod.quantity > product.stock) {
-            console.log("Quantity mayor que stock")
 
             productsNotBought.push(product._id); // TODO: Quiza pushear todo el producto
 
@@ -78,12 +77,19 @@ export default class CartService {
 
         await this.productService.updateProduct(product._id, {stock: product.stock - prod.quantity})
 
-        productsBought.push({productId: product._id, quantity: prod.quantity}) // TODO: Quiza agregarlo todo
+        productsBought.push({productId: product._id, quantity: prod.quantity})  /* TODO: Quiza agregarlo todo */
 
         total += (prod.quantity * product.price)
         }
 
         return { productsBought, total, productsNotBought }
+    }
+
+    async deleteProductsFromCart(cartId, products) {
+        for (let product of products) {
+        console.log(cartId, product.productId)
+        await this.deleteProductFromCart(cartId, product.productId.toString())
+        }
     }
 
 }
